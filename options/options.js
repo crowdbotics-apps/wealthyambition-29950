9a08281a324df5cf266269b4b505b7e8f0f05266
Crowdbotics <team@crowdbotics.com>
2021-08-19 17:50:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wealthyambition_29950Identifier",
+  url: "https://wealthyambition_29950Identifier.botics.co",
+  api: "https://wealthyambition_29950Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
